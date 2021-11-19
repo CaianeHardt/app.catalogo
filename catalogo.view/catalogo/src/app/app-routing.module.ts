2b239a7510eb0ac29f24a5c47599a948491e0262
CaianeHardt <caianehardt@gmail.com>
2021-11-19 19:56:12 -0300

    Add users module

--- catalogo.view/catalogo/src/app/app-routing.module.ts
+++ catalogo.view/catalogo/src/app/app-routing.module.ts
@@ -4,17 +4,18 @@ import { SignInComponent } from './home/signin/signin.component';
 
 const routes: Routes = [
   {
-    path:'',
-    component: SignInComponent
+    path: '',
+    component: SignInComponent,
   },
-  // {
-  //   path:'/users/login',
-  //   component: SignInComponent
-  // }
+  {
+    path: 'users',
+    loadChildren: () =>
+      import('./pages/users/users.module').then((m) => m.UsersModule),
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
