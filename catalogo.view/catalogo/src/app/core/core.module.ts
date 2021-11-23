f4bb8868f9b9f94a2f34477b6f2c70a40ac36631
CaianeHardt <caianehardt@gmail.com>
2021-11-22 22:12:00 -0300

    Add core module

---
+++ catalogo.view/catalogo/src/app/core/core.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ModuleWithProviders } from '@angular/compiler/src/core';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthService } from './services/auth.service';
+import { AuthenticationInterceptor } from './helpers/authentication.interceptor';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    BrowserAnimationsModule,
+    HttpClientModule
+  ],
+  exports: [
+    BrowserAnimationsModule,
+    HttpClientModule
+  ]
+})
+export class CoreModule { 
+  static forRoot(): ModuleWithProviders {
+    return {
+      ngModule: CoreModule,
+      providers: [
+        AuthService,
+        { provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptor, multi: true },
+      ]
+    };
+  }
+}
